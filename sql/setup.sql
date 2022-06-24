38cbb97f2dd5b9c1ba19050d93e633e0fd19c336
Will Gundy <willgunderson503@gmail.com>
2022-06-24 16:35:09 -0700

    added authenticate middleware

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS github_users;
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS github_users;
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
